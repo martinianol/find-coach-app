7886c705a5a0599bfbb9534af9db541ef150a704
martinianol <martinianol@outlook.com>
2022-03-06 17:58:03 -0300

    Add getters to CoachList

--- src/components/pages/coaches/CoachList.vue
+++ src/components/pages/coaches/CoachList.vue
@@ -7,13 +7,25 @@
       <button>Refresh</button>
       <router-link to="/register">Register as a coach</router-link>
     </div>
-    <ul>LIST OF COACHES</ul>
+    <ul v-if="hasCoaches">LIST OF COACHES      
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName}}
+      </li>
+    </ul>
+    <h3 v-else>No Coaches Found</h3>
   </section>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/getAll']
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches']
+    }
+  }
 }
 </script>
 
--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -1,1 +1,8 @@
-export default {}
+export default {
+  getAll (state) {
+    return state.coaches;
+  },
+  hasCoaches(state) {
+    return state.coaches && state.coaches.length > 0;
+  }
+}
