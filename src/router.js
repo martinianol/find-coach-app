653852925e1ae29c4322bb6924bd4df9c1bcea63
martinianol <martinianol@outlook.com>
2022-03-06 15:51:34 -0300

    Add Route Page Components

--- src/router.js
+++ src/router.js
@@ -1,16 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import CoachDetails from './components/pages/coaches/CoachDetails.vue';   
+import CoachList from './components/pages/coaches/CoachList.vue';   
+import CoachRegistration from './components/pages/coaches/CoachRegistration.vue';
+import ContatCoach from './components/pages/requests/ContatCoach.vue';
+import RequestsReceived from './components/pages/requests/RequestsReceived.vue';
+import NotFound from './components/pages/NotFound.vue';   
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, children: [
-            { path: '/contact', component: null }
+        { path: '/coaches', component: CoachList },
+        { path: '/coaches/:id', component: CoachDetails, children: [
+            { path: '/contact', component: ContatCoach }
         ] },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null },
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound },
     ]    
 });
 
