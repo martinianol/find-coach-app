2814721eab05e4c46f0e9a1d52c087fe79450030
martinianol <martinianol@outlook.com>
2022-03-08 15:43:49 -0300

    Add PUT https to firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,30 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
     //const id = "c" + (context.getters.getAll.length+1)
+    const userId = context.rootGetters.userId;
+
     const newCoach = {
-      id: context.rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       areas: payload.areas,
       description: payload.desc,
       hourlyRate: payload.rate
+    };
+
+    const response = await fetch(`https://vue-coach-app-4a323-default-rtdb.firebaseio.com/coache/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(newCoach)
+    });
+
+    //const responseData = await response.json()
+
+    if (!response.OK) {
+      //We'll do somehting later with later
     }
-    context.commit('addCoach', newCoach); 
+
+    context.commit('addCoach', {
+      ...newCoach,
+      id: userId
+    }); 
   }
 }
