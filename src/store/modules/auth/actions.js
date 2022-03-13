0d413685d907d0856c9d86ad1fa74aeade26c370
martinianol <martinianol@outlook.com>
2022-03-13 15:24:14 -0300

    Add Auth to store

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+
+export default {
+  async login(context, payload) {
+    //const apiKey = process.env.COACH_APP_API_KEY
+
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBzn9J72IL6HIGCIGEKC3hfUHnmcRIyzAs`, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+    })
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData)
+      const error = new Error(response.message || 'Failed to authenticate');
+      throw error;
+    }
+    
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    })
+
+  },
+  async signup(context, payload) {
+    const apiKey = process.env.COACH_APP_API_KEY
+    console.log(apiKey)
+    console.log(payload.password)
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBzn9J72IL6HIGCIGEKC3hfUHnmcRIyzAs`, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+    })
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData)
+      const error = new Error(response.message || 'Failed to authenticate');
+      throw error;
+    }
+    
+    console.log(responseData)
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    })
+  
+  }
+}
